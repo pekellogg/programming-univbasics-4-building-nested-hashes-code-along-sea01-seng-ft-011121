e027b851b9f6d1321db59eb1b19e3e48a6629434
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 08:09:43 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,6 +1,13 @@
+# def first_challenge
+#    epic_tragedy = {
+#      montague: {},
+#      capulet: {}
+#    }
+# end
+
 def first_challenge
    epic_tragedy = {
-     montague: {},
-     capulet: {}
+     :montague => {},
+     :capulet => {}
    }
 end
